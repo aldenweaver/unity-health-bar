6596d945cc99a23d59e95b6ef3f36008842bc4b0
aldenweaver <aaldenweaver@gmail.com>
2015-01-22 17:30:07 -0800

    Completed health bar scripting

--- unity-health-bar/Assets/PlayerScript.cs
+++ unity-health-bar/Assets/PlayerScript.cs
@@ -8,10 +8,43 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour
 {
     public float speed;
- 
+    public RectTransform healthTransform;
+    private float yPos;
+    private float maxXValue; // 100% health = startPos - barLength
+    private float minXValue;
+
+    private int currentHealth;
+
+    private int CurrentHealth {
+        get {return currentHealth;}
+        set {
+            currentHealth = value;
+            HandleHealth();
+        }
+    }
+
+    public float maxHealth;
+
+    public Text healthText;
+
+    // access
+    public Image visualHealth;
+
+    public float coolDown;
+    private bool onCoolDown;
+
+
+
     // Use this for initialization
     void Start()
     {   
+        yPos = healthTransform.position.y;
+        maxXValue = healthTransform.position.x;
+        minXValue = healthTransform.position.x - healthTransform.rect.width;
+
+        currentHealth = maxHealth;
+
+        onCoolDown = false;
 
     }
 
@@ -21,10 +54,33 @@ public class PlayerScript : MonoBehaviour
         HandleMovement();
     }
 
+    private void HandleHealth() {
+        handleText.text = "Health: " + currentHealth;
+        float currentXValue = MapValues(currentHealth, 0, maxHealth, minXValue, maxXValue);
+
+        // Transform health bar into correct osition
+        healthTransform.position = new Vector3(currentXValue, yPos); 
+
+        // If > 50% health, then transform R
+        if (currentHealth > maxHealth / 2) {
+            visualHealth.color = new Color32((byte)MapValues(currentHealth, maxHealth/2, maxHealth, 255, 0), 255, 0, 255);
+
+        } 
+        // If < 50% health, then transform G
+        else {
+            visualHealth.color = new Color32(255, (byte)MapValues(currentHealth, 0, maxHealth/2, 0, 255), 0, 255);
+        }
+    }
+
+    IEnumerator CoolDownDamage() {
+        onCD = true;
+        yield return new waitForSeconds(coolDown);
+        onCD = false;
+
+    }
 
     // Handles the player's movement
-    private void HandleMovement()
-    {   
+    private void HandleMovement() {   
         // Used for making the movement framerate independent
         float translation = speed * Time.deltaTime;
 
@@ -33,4 +89,32 @@ public class PlayerScript : MonoBehaviour
         // Makes frame rate independent
         transform.Translate(new Vector3(Input.GetAxis("Horizontal") * translation, 0, Input.GetAxis("Vertical") * translation));
     }
+
+    void OnTriggerStay(Collider other) {
+        if (other.name = "Damage") {
+            if(!onCD && currentHealth > 0) {
+                StartCoroutine(CoolDownDamage());
+                CurrentHealth -= 1;
+            }
+        }
+
+        if (other.name = "Health") {
+            if(!onCD && currentHealth > maxHealth) {
+                StartCoroutine(CoolDownDamage());
+                CurrentHealth += 1;
+            }
+        }
+    }
+
+
+    // health   0   50  100
+    // posX     -10 -5  0
+    private float MapValues(float x, float inMin, float inMax, float outMin, float outMax) {
+        return ((x -inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
+    }
+
+
+
+
 }
